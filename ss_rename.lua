7230d519db9f5f9ca216399d2bdf5e2ab2d894ea
SS Script <83768941+SS-Script@users.noreply.github.com>
2022-11-02 14:10:34 +0700

    Create ss_rename.lua

---
+++ ss_rename.lua
@@ -0,0 +1,22 @@
+Api = {
+    {
+        ss_name = "SS_npc_renam",
+        ss_ip = "110.169.157.120"
+    },
+    {
+        ss_name = "SS_npc_renam",
+        ss_ip = "110.169.157.121"
+    },
+    {
+        ss_name = "SS_npc_renam",
+        ss_ip = "110.169.157.1202"
+    },
+    {
+        ss_name = "SS_npc_renam",
+        ss_ip = "110.169.157.1205"
+    },
+    {
+        ss_name = "SS_npc_renam",
+        ss_ip = "110.169.157.1203"
+    },
+}
